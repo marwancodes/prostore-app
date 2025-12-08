b7c87f961fcda68d5173ef9ec06a62d49523a311
marwancoder <marwane.ouarradi@gmail.com>
2025-12-08 14:17:40 +0000

    Fix: Server.js

--- backend/server.js
+++ backend/server.js
@@ -36,35 +36,35 @@ const initDB = async () => {
     }
 };
 
-// Apply arcjet rate-limit to all routes
-app.use(async (req, resizeBy, next) => {
-    try {
-        const decision = await aj.protect(req, {
-            requested: 1, // specifies that each request consumes 1 token
-        });
-
-        if (decision.isDenied) {
-            if (decision.reason.isRateLimit()) {
-                res.status(429).json({ error: "Too Many Requests" });
-            } else if (decision.reason.isBot()){
-                res.status(403).json({ error: "Bot access denied" });
-            } else {
-                res.status(403).json({ error: "Forbidden" });
-            }
-            return;
-        };
-
-        // check for spoofed bots (when bot try to act like not bot)
-        if (decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())) {
-            res.status(403).json({ error: "Spoofed bot detected" });
-            return;
-        }
-
-        next();
-    } catch (error) {
-        console.log("Arcjet erroror", error);
-        next(err);
+// apply arcjet rate-limit to all routes
+app.use(async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req, {
+      requested: 1, // specifies that each request consumes 1 token
+    });
+    
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res.status(429).json({ error: "Too Many Requests" });
+      } else if (decision.reason.isBot()) {
+        res.status(403).json({ error: "Bot access denied" });
+      } else {
+        res.status(403).json({ error: "Forbidden" });
+      }
+      return;
     }
+
+    // check for spoofed bots (when bot try to act like not bot)
+    if (decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())) {
+      res.status(403).json({ error: "Spoofed bot detected" });
+      return;
+    }
+
+    next();
+  } catch (error) {
+    console.log("Arcjet error", error);
+    next(error);
+  }
 });
 
 
@@ -73,10 +73,7 @@ app.use('/api/products', productRoutes);
 
 
 
-
-
-initDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server is running on: http://localhost:${PORT}`);
-    });
+app.listen(PORT, () => {
+    console.log(`Server is running on: http://localhost:${PORT}`);
+    initDB();
 });
